a3e3df7e79302b8d7ffc349c286df163b7128328
SevJ6 <iansevier04@gmail.com>
2025-01-31 12:00:35 -0700

    Quit relying on the Paper API

--- src/main/java/me/ian/mixin/MixinManager.java
+++ src/main/java/me/ian/mixin/MixinManager.java
@@ -4,28 +4,28 @@ import net.bytebuddy.agent.ByteBuddyAgent;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.instrument.Instrumentation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class MixinManager {
     private static final List<Mixin> mixins = new ArrayList<>();
     private static ClassLoader serverClassLoader;
 
-    public static void init(JavaPlugin plugin) {
-        serverClassLoader = plugin.getServer().getClass().getClassLoader();
+    public static void init(ClassLoader classLoader) {
+        serverClassLoader = classLoader;
         ByteBuddyAgent.install();
-        applyMixins(ByteBuddyAgent.getInstrumentation(), plugin);
-        plugin.getLogger().info("Applied " + mixins.size() + " mixin(s)");
+        applyMixins(ByteBuddyAgent.getInstrumentation());
+        Logger.getGlobal().info("[SimpleMixins] Applied " + mixins.size() + " mixin(s)");
     }
 
     public static void register(Mixin mixin) {
         mixins.add(mixin);
     }
 
-    private static void applyMixins(Instrumentation instrumentation, JavaPlugin plugin) {
+    private static void applyMixins(Instrumentation instrumentation) {
         AgentBuilder agentBuilder = new AgentBuilder.Default()
                 .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
                 .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
@@ -37,11 +37,11 @@ public class MixinManager {
                     .and(ElementMatchers.declaresMethod(mixin.getMethodMatcher()))
                     .transform((builder, type, classLoader, module, pd) -> {
                         if (classLoader != serverClassLoader) {
-                            plugin.getLogger().warning("Skipping classloader: " + classLoader);
+                            Logger.getGlobal().warning("[SimpleMixins] Skipping classloader: " + classLoader);
                             return builder;
                         }
 
-                        plugin.getLogger().info("Successfully transformed class: " + type.getName());
+                        Logger.getGlobal().info("[SimpleMixins] Successfully transformed class: " + type.getName());
                         return builder.visit(Advice.to(mixin.getAdviceClass()).on(mixin.getMethodMatcher()));
                     });
         }
